564bc881f6491fc9c002d8779ce671a59b9c3f55
Wagner Xu <wagnerhsu@hotmail.com>
2021-11-28 16:11:03 +0800

    Add file

---
+++ MyCode/12/Search-StackOverflow.ps1
@@ -0,0 +1,50 @@
+##############################################################################
+##
+## Search-StackOverflow
+##
+## From PowerShell Cookbook (O'Reilly)
+## by Lee Holmes (http://www.leeholmes.com/guide)
+##
+##############################################################################
+
+<#
+
+.SYNOPSIS
+
+Searches Stack Overflow for PowerShell questions that relate to your
+search term, and provides the link to the accepted answer.
+
+
+.EXAMPLE
+
+PS > Search-StackOverflow upload ftp
+Searches StackOverflow for questions about how to upload FTP files
+
+.EXAMPLE
+
+PS > $answers = Search-StackOverflow.ps1 upload ftp
+PS > $answers | Out-GridView -PassThru | Foreach-Object { start $_ }
+
+Launches Out-GridView with the answers from a search. Select the URLs
+that you want to launch, and then press OK. PowerShell then launches
+your default web browser for those URLs.
+
+#>
+
+Set-StrictMode -Off
+Add-Type -Assembly System.Web
+
+$query = $args -join " "
+$query = [System.Web.HttpUtility]::UrlEncode($query)
+
+## Use the StackOverflow API to retrieve the answer for a question
+$url = "https://api.stackexchange.com/2.0/search?order=desc&sort=relevance" +
+        "&pagesize=5&tagged=powershell&intitle=$query&site=stackoverflow"
+$question = Invoke-RestMethod $url
+
+## Now go through and show the questions and answers
+$question.Items | Where-Object accepted_answer_id | Foreach-Object {
+    "Question: " + $_.Title
+    "https://www.stackoverflow.com/questions/$($_.accepted_answer_id)"
+    ""
+}
